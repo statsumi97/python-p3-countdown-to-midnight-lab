7ad27a46048efe2a4147e5cf29c16f6b31d2a718
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-12-14 10:57:54 -0500

    final product

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,23 @@
 # your code goes here!
+import time
+
+def countdown(start_number = 10):
+    number = start_number
+
+    while number >= 1:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+
+    print('HAPPY NEW YEAR!')
+    return None
+
+def countdown_with_sleep(start_number = 10):
+    number = start_number
+
+    while number >= 1:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+        time.sleep(1)
+
+    print('HAPPY NEW YEAR!')
+    return None
